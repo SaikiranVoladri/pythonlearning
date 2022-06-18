3c39792b7768f81df5a2775eb9965639b4bcad0f
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-06-18 10:35:14 +0530

    Python beginners level
    These codes are the basic level of the python language

---
+++ APNACOLLEGE/myfirstprogram.py
@@ -0,0 +1,121 @@
+# python basics 
+# print("hello world!")
+#VARIABLES==============================
+# name="saikiran"
+# age=18
+
+# print("name = "+ name)
+# print(age )
+#VARIABLES==============================
+#EXCERCISE==============================
+# name='TONT STARK'
+# age = str(51)
+# print(name)
+# print("age ="+ age)
+# print("tony is genious")
+#EXCERCISE==============================
+#INPUT =================================
+# name = input("enter your name")
+# age= input("enter your age")
+# print("hello "+ name)
+# print(" Your age =",age)
+#INPUT =================================
+# name = input("enter your name::")
+# specs= input("enter your specification")
+# print("name = ",name)
+# print(name ,"is an",specs)
+#TYPE CONVERSION =================================
+# old_age = 18
+# new_age = old_age+2
+# print(float(new_age))
+# print(new_age)
+# ..............................................
+# num=10
+# print(float(num))
+# print(bool(num))
+# print(str(num))
+#SUM OF 2 NUMBERS  =================================
+# num1=int(input("enter first number"))
+# num2=int(input("enter second number"))
+# num3=num1+num2;
+# print("sum = ",num3)
+#STRING FUNCTIONS  =================================
+# name= 'SaiKiranReddy'
+# print(name.lower())
+# print(name.upper())
+# print(name.find("R"))
+# print(name.replace("Reddy","goud"))
+# print(name)
+#ARITHEMATIC OPERATERS =================================
+# print(5+5)
+# print(5-5)
+# print(5*5)
+# print (int(5/5))
+# print(5%5)
+#LOGICAL OPERATERS =================================
+# print(3>2)
+# print(3<2)
+# print(2>3 or 3>2)
+# print(3>2 and 3<2)
+# print(not 3>2 )
+#IF ELSE STATEMENTS =================================
+# age = int(input ("enter age rey"))
+# if age >= 18:
+#     print(" hey you are 18+")
+# elif age<10 and age >5:
+#     print("ho you are still schooler")
+# else:
+#     print("you are  a small kid re")
+# CALCULATOR =================================
+# num1=int(input("enter first number"))
+# operater = input("choose operater(+ - * / % :")
+# num2 =int(input("enter second number"))
+# if operater == '+' :
+#     print(   num1,"+",num2,"=",num1 + num2)
+# elif operater == '-' :
+#     print(num1,"-",num2,"=",num1 - num2)
+# elif operater == '*' :
+#     print(num1,"*",num2,"=",num1 * num2)
+# elif operater == '/' :
+#     print(num1,"/",num2,"=",num1 / num2)
+# elif operater == '%' :
+#     print(num1,"%",num2,"=", num1 % num2)
+# else:
+#   print("invalid operater")
+# RANGE WHILELOOP  =================================  
+# num = range(5)
+# print(num)
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+# BREAK AND CONTINUE  =================================  
+# names=["s","a","i"]
+# for students in names:
+#     if(students == "a"):
+#      break
+#     print(students)
+# students = ["ram", "shyam", "kishan", "radha", "radhika"]
+
+# for student in students:
+#    if(student == "radha"):
+#        break
+#    print(student)
+# LISTS[] tuple() dictionary{}  =================================  
+# _list=[1,2,3,4,5,6]
+# print(len(_list))
+# print(_list.index(3))
+# _set={1,2,3,4}
+# print(_list)
+# _dict={"r","t"}
+# print(_dict)
+# marks={"1":20,"2":22,"3":11}
+# print(marks("1"))
+
+
+
+
+
+#FuNcTiOnS''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
